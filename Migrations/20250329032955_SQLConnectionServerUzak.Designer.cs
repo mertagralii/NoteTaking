65a22415b87604e0e8eb3d4b6e506751aaf46991
Mert Ağralı <my_mert07hotmail.com>
2025-03-29 06:34:06 +0300

    fix

--- Migrations/20250329032955_SQLConnectionServerUzak.Designer.cs
+++ Migrations/20250329032955_SQLConnectionServerUzak.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NoteTaking.Data;
 
@@ -11,9 +12,11 @@ using NoteTaking.Data;
 namespace NoteTaking.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250329032955_SQLConnectionServerUzak")]
+    partial class SQLConnectionServerUzak
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
